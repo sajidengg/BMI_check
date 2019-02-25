82f19a085bb2e2b38ed23a889c8e03e2eff6b8e7
ss18aef <ss18aef@ENT-D410-25.herts.ac.uk>
2019-02-25 14:03:54 +0000

    created variable222222

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -17,6 +17,8 @@ public class JavaApplication3 {
     public static void main(String[] args) {
         System.out.println("hello");
         int a=20;
+        int b=30;
+        int c=1;
     }
     
 }
