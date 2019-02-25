59b1a867530230f68efb6b1d8d79a1c1ef895dba
ss18aef <ss18aef@ENT-D410-25.herts.ac.uk>
2019-02-25 14:06:22 +0000

    removed value

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -16,7 +16,7 @@ public class JavaApplication3 {
      */
     public static void main(String[] args) {
         System.out.println("hello");
-        int a=20;
+        int a;
         int b=30;
         int c=1;
     }
